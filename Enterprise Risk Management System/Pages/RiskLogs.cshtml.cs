3197b8d7cb84aee91317d5de3c32244ba1cc0f0f
Sibabalwe Peter <sibabalwepeter2848@gmail.com>
2025-06-02 21:01:43 +0200

    reports

---
+++ Enterprise Risk Management System/Pages/RiskLogs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Enterprise_Risk_Management_System.Pages
+{
+    public class RiskLogsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
